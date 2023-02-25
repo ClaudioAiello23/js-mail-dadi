4b6e4c6662adac073b6058894de164f27df7b95b
ClaudioAiello23 <claudio.aiello23@gmail.com>
2023-02-25 16:51:53 +0100

    sistemazione html+css risultato Gioco Dadi

--- Gioco dei dadi/js/main.js
+++ Gioco dei dadi/js/main.js
@@ -1,21 +1,57 @@
 'use strict';
 
+/*impostare un Alert per iniziare il gioco;*/
 alert('Sei pronto? Scopri se hai vinto al GIOCO DEI DADI');
 
+/*creare una variabile playerNumber che crei un numero random
+da 1 a 6 per il giocatore;*/
 alert('Ecco il tuo numero');
 const playerNumber = Math.floor (Math.random()*6);
 alert('Il tuo numero è: '+ playerNumber);
 console.log("Numero giocatore: ", playerNumber);
 
+/*creare una variabile cpuNumber che crei un numero random
+da 1 a 6 per la cpu;*/
 alert('Ecco il numero della CPU');
 const cpuNumber = Math.floor (Math.random()*6);
 alert('Il numero della CPU è: '+ cpuNumber);
 console.log("Numero cpu: ", cpuNumber);
 
+/*Creare una variabile finalNumber che faccia la differenza tra il
+numero giocatore e il numero cpu;*/
 const finalNumber = playerNumber - cpuNumber;
 console.log("Differenza N.Giocatore - N.Cpu: ", finalNumber);
 
 
+/*Impostare una IF con le seguenti casistiche:
+- se la differenza tra playerNumber e cpuNumber è maggiore di zero,
+mostrare un alert che premia il giocatore come vincitore;
+- se la differenza tra playerNumber e cpuNumber è maggiore di zero,
+mostrare un alert che premia il giocatore come vincitore;
+- se la differenza tra playerNumber e cpuNumber è maggiore di zero,
+mostrare un alert che premia il giocatore come vincitore;*/
+if (finalNumber > 0){
+    alert('SIIIIIIIIII, HAI VINTO!!!!');
+    document.getElementById('final_message').innerHTML = 'SIIIIIIIIII, HAI VINTO !!!!';
+    console.log("ha vinto il giocatore");
+    
+} else if (finalNumber < 0){
+    alert('PECCATO, HA VINTO LA CPU! !!!!');
+    document.getElementById('final_message').innerHTML = 'PECCATO, HA VINTO LA CPU ! !!!!';
+    console.log("ha vinto la Cpu");
+
+} else {
+    alert('ABBIAMO PAREGGIATO, 1-1......ICS!!!!');
+    document.getElementById('final_message').innerHTML = 'ABBIAMO PAREGGIATO, 1-1......ICS !!!!';
+    console.log("pareggio");
+
+}
+
+document.getElementById('player_number').innerHTML = `Numero Giocatore: ${playerNumber}`;
+document.getElementById('cpu_number').innerHTML = `Numero Cpu: ${cpuNumber}`;
+
+
+// SWITCH NON FUNZIONA CORRETTAMENTE - CHIEDERE
 // switch (finalNumber){
 
 //     case finalNumber < 0 :
@@ -33,17 +69,3 @@ console.log("Differenza N.Giocatore - N.Cpu: ", finalNumber);
 //     console.log("pareggio");
     
 // }
-
-if (finalNumber > 0){
-    alert('SIIIIIIIIII, HAI VINTO!!!!');
-    console.log("ha vinto il giocatore");
-    
-} else if (finalNumber < 0){
-    alert('PECCATO, HA VINTO LA CPU! !!!!');
-    console.log("ha vinto la Cpu");
-
-} else {
-    alert('ABBIAMO PAREGGIATO, 1-1......ICS!!!!');
-    console.log("pareggio");
-
-}

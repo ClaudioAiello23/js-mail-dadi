3e97e387aa4f379553e224f4d51c280b7374cb77
ClaudioAiello23 <claudio.aiello23@gmail.com>
2023-02-25 16:00:14 +0100

    risoluzione esercizio Gioco Dadi - file JS

--- Gioco dei dadi/READMEdadi.md
+++ Gioco dei dadi/READMEdadi.md
@@ -1,5 +1,9 @@
 README - ESERCIZIO "GIOCO DEI DATI"
-- creare il dado come lista di numeri da 1 a 6
-- creare un ciclo FOR
-- chiedere al giocatore di premere un pulsante che generi un numero per lo stesso giocatore e un numero per il computer (random).
-- mettere la condizione che se il numero scelto dal giocatore è > del numero random, il giocatore ha vinto; in caso contrario, il giocatore ha perso; valutare anche il caso che i due numeri siano uguali.
+- impostare un Alert per iniziare il gioco
+- creare una variabile playerNumber che crei un numero random
+da 1 a 6  (vedere se impostare anche un console.log).
+- creare una variabile cpuNumber che crei un numero random
+da 1 a 6 (vedere se impostare anche un console.log).
+- impostare una switch (o una if) con le seguenti casistiche:
+se il playerNumber è maggiore del cpuNumber mostrare un alert
+che premia il giocatore come vincitore; se il playerNumber è minore del cpuNumber mostrare un alert che comunichi la vittoria della Cpu; se i due numeri sono uguali (default) mostrare un alert che comunichi il pareggio.
--- Gioco dei dadi/js/main.js
+++ Gioco dei dadi/js/main.js
@@ -1,1 +1,49 @@
 'use strict';
+
+alert('Sei pronto? Scopri se hai vinto al GIOCO DEI DADI');
+
+alert('Ecco il tuo numero');
+const playerNumber = Math.floor (Math.random()*6);
+alert('Il tuo numero è: '+ playerNumber);
+console.log("Numero giocatore: ", playerNumber);
+
+alert('Ecco il numero della CPU');
+const cpuNumber = Math.floor (Math.random()*6);
+alert('Il numero della CPU è: '+ cpuNumber);
+console.log("Numero cpu: ", cpuNumber);
+
+const finalNumber = playerNumber - cpuNumber;
+console.log("Differenza N.Giocatore - N.Cpu: ", finalNumber);
+
+
+// switch (finalNumber){
+
+//     case finalNumber < 0 :
+//     // alert('SIIIIIIIIII, HAI VINTO!!!!');
+//     console.log("hai vinto");
+//     break;
+
+//     case finalNumber < 0 :
+//     // alert('PECCATO, HA VINTO LA CPU! !!!!');
+//     console.log("hai perso");
+//     break;
+
+//     default: 
+//     // alert('ABBIAMO PAREGGIATO, 1-1......ICS!!!!');
+//     console.log("pareggio");
+    
+// }
+
+if (finalNumber > 0){
+    alert('SIIIIIIIIII, HAI VINTO!!!!');
+    console.log("ha vinto il giocatore");
+    
+} else if (finalNumber < 0){
+    alert('PECCATO, HA VINTO LA CPU! !!!!');
+    console.log("ha vinto la Cpu");
+
+} else {
+    alert('ABBIAMO PAREGGIATO, 1-1......ICS!!!!');
+    console.log("pareggio");
+
+}
